0ec83e2aa3c1d6e4c05270baaecabc6aa3b71986
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-06-17 04:32:14 +0200

    anime-game-launcher: 3.14.1 -> 3.14.2

--- pkgs/anime-game-launcher/unwrapped.nix
+++ pkgs/anime-game-launcher/unwrapped.nix
@@ -18,44 +18,44 @@
   customIcon ? null,
 }:
 with lib;
-  rustPlatform.buildRustPackage rec {
-    pname = "anime-game-launcher";
-    version = "3.14.1";
+rustPlatform.buildRustPackage rec {
+  pname = "anime-game-launcher";
+  version = "3.14.2";
 
-    src = fetchFromGitHub {
-      owner = "an-anime-team";
-      repo = "an-anime-game-launcher";
-      rev = version;
-      hash = "sha256-2TKEmoNJNKWjM7c6y9NCi9oEmRe4zauB7EuEs8UN7ec=";
-      fetchSubmodules = true;
-    };
+  src = fetchFromGitHub {
+    owner = "an-anime-team";
+    repo = "an-anime-game-launcher";
+    rev = version;
+    hash = "sha256-tJrUk6PhTdP9XBTahsuv8JAE7K865ZLXxNez/XB86bQ=";
+    fetchSubmodules = true;
+  };
 
-    prePatch = optionalString (customIcon != null) ''
-      rm assets/images/icon.png
-      cp ${customIcon} assets/images/icon.png
-    '';
+  prePatch = optionalString (customIcon != null) ''
+    rm assets/images/icon.png
+    cp ${customIcon} assets/images/icon.png
+  '';
 
-    useFetchCargoVendor = true;
-    cargoHash = "sha256-a4LAWKnltTE50rA/aXvQGMMMUaKz7Xg+YxMH5bcSZns=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-LpOCiyZZi4eEniE1TY4LIpFKM0SO6Tvofj0yZuqdcl4=";
 
-    nativeBuildInputs = [
-      cmake
-      protobuf
-      glib
-      gobject-introspection
-      gtk4
-      pkg-config
-      wrapGAppsHook4
-    ];
+  nativeBuildInputs = [
+    cmake
+    protobuf
+    glib
+    gobject-introspection
+    gtk4
+    pkg-config
+    wrapGAppsHook4
+  ];
 
-    buildInputs = [
-      gdk-pixbuf
-      gsettings-desktop-schemas
-      libadwaita
-      librsvg
-      openssl
-      pango
-    ];
+  buildInputs = [
+    gdk-pixbuf
+    gsettings-desktop-schemas
+    libadwaita
+    librsvg
+    openssl
+    pango
+  ];
 
-    passthru = {inherit customIcon;};
-  }
+  passthru = { inherit customIcon; };
+}
